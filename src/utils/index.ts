cbe4ec2520ed721f70815175e23e66f17e3c7801
Vinicius Soranço <vinicius.soranco@levty.com>
2020-09-18 10:16:31 -0300

    voltando a versao anterior

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,12 @@
-import html2canvas from 'html2canvas-render-offscreen';
+import html2canvas from 'html2canvas';
 
 export function DownloadElement(ref: HTMLElement, frameName?: string){
   html2canvas(ref, { 
     logging: true, 
+    allowTaint: true,
     useCORS: true, 
     proxy: process.env.REACT_APP_PROXY_URL!, 
     scale: window.innerWidth < 660 ? 2 : 1,
-    width: 500,
-    height: 500
   }).then((canvas:any) => {
     let a = document.createElement('a');
     a.href=canvas.toDataURL("image/png");
