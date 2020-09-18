41b697158374d86fb7e0e39aad4492a86fdf9649
Vinicius Soranço <vinicius.soranco@levty.com>
2020-09-17 23:08:43 -0300

    ajustes no download

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,14 @@
 import html2canvas from 'html2canvas';
 
 export function DownloadElement(ref: HTMLElement, frameName?: string){
-  html2canvas(ref, { logging: true, allowTaint: true, useCORS: true, proxy: process.env.REACT_APP_PROXY_URL!, scale: window.innerWidth < 660 ? 2 : 1 }).then(canvas => {
-    var a = document.createElement('a');
+  html2canvas(ref, { 
+    logging: true, 
+    allowTaint: true,
+    useCORS: true, 
+    proxy: process.env.REACT_APP_PROXY_URL!, 
+    scale: window.innerWidth < 660 ? 2 : 1
+  }).then(canvas => {
+    let a = document.createElement('a');
     a.href=canvas.toDataURL("image/png");
     a.download = `moldura-${frameName}.png`
     a.click();
