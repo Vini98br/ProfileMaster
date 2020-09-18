cd4f48051f0217e6c124814e44ed42400724ec33
Vinicius Soranço <vinicius.soranco@levty.com>
2020-09-18 09:30:23 -0300

    correção imagem branca no celular

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,12 @@
-import html2canvas from 'html2canvas';
+import html2canvas from 'html2canvas-render-offscreen';
 
 export function DownloadElement(ref: HTMLElement, frameName?: string){
   html2canvas(ref, { 
     logging: true, 
-    allowTaint: true,
     useCORS: true, 
     proxy: process.env.REACT_APP_PROXY_URL!, 
     scale: window.innerWidth < 660 ? 2 : 1
-  }).then(canvas => {
+  }).then((canvas:any) => {
     let a = document.createElement('a');
     a.href=canvas.toDataURL("image/png");
     a.download = `moldura-${frameName}.png`
